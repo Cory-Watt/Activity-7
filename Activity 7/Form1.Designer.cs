3176390fb360ee04d890bfb9f946da03d5935eac
Cory-Watt <106133667+Cory-Watt@users.noreply.github.com>
2022-06-05 17:07:23 -0400

    Add files via upload

---
+++ Activity 7/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Activity_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.numTerms = new System.Windows.Forms.Label();
+            this.calculate = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.45693F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.54307F));
+            this.tableLayoutPanel1.Controls.Add(this.calculateBtn, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.userInput, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(267, 100);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter # of terms:";
+            // 
+            // userInput
+            // 
+            this.userInput.Location = new System.Drawing.Point(95, 3);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(169, 20);
+            this.userInput.TabIndex = 1;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(3, 53);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(85, 44);
+            this.calculateBtn.TabIndex = 1;
+            this.calculateBtn.Text = "Calculate";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // numTerms
+            // 
+            this.numTerms.AutoSize = true;
+            this.numTerms.Location = new System.Drawing.Point(9, 130);
+            this.numTerms.Name = "numTerms";
+            this.numTerms.Size = new System.Drawing.Size(0, 13);
+            this.numTerms.TabIndex = 1;
+            // 
+            // calculate
+            // 
+            this.calculate.AutoSize = true;
+            this.calculate.Location = new System.Drawing.Point(9, 171);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(0, 13);
+            this.calculate.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 219);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.numTerms);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(325, 258);
+            this.MinimumSize = new System.Drawing.Size(325, 258);
+            this.Name = "Form1";
+            this.Text = "Approximate PI";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.Label numTerms;
+        private System.Windows.Forms.Label calculate;
+    }
+}
+
---
+++ Activity 7/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            double pi = 4;
+            double circumference = 3;
+            double denominator = 3;
+
+            int input = int.Parse(userInput.Text);
+            
+            while (denominator <= 200)
+            {
+                if (circumference % 2 != 0)
+                {
+                    pi -= Convert.ToDouble(4) / denominator;
+                    circumference++;
+                }
+                else
+                {
+                    pi += Convert.ToDouble(4) / denominator;
+                    circumference++;
+                }
+                denominator += 2;
+            }
+            numTerms.Text = "Approximate value of pi after " + input + " terms.";
+            calculate.Text = " = " + pi;
+        }
+    }
+}
